3eae63e11609dc8137bc25ceed8b0a7afdc1060b
mhchoi <mh.choi@altimedia.com>
2022-02-16 20:21:28 +0900

    state라는 Component의 객체로 하위 컴포넌틑에 값 전달하기

--- src/App.js
+++ src/App.js
@@ -5,12 +5,26 @@ import Subject from './components/Subject';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // state는 Component의 로컬 변수라고 생각한다.
+    this.state = {
+      subject: { title:'WEB', sub:'World Wide Web'},
+      contents: [
+        {id:1, title:'HTML', desc:'HTML is Hyper Text ...'},
+        {id:2, title:'CSS', desc:'CSS is for design ...'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive ...'}
+      ]
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web"></Subject>
-        <Subject title="React" sub="React 배우기"></Subject>
-        <TOC></TOC>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>
     );
