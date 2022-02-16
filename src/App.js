c3bc00e4a3db9b6c972ab27bc4a9cfaf16c3a9c7
mhchoi <mh.choi@altimedia.com>
2022-02-16 19:43:46 +0900

    React의 props 사용하기 ( html의 attribute )

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ class Subject extends Component {
   render() {
     return (
       <header>
-        <h1>WEB</h1>
-        world wide web!
+        {/* React에는 attribute를 props라고 부른다. */}
+        <h1>{this.props.title}</h1> 
+        {this.props.sub}
       </header>
     );
   }
@@ -30,8 +31,8 @@ class Content extends Component {
   render() {
     return (
       <article>
-        <h2>HTML</h2>
-        HTML is HyperText Markup Language
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
       </article>
     )
   }
@@ -41,9 +42,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title="WEB" sub="world wide web"></Subject>
+        <Subject title="React" sub="React 배우기"></Subject>
         <TOC></TOC>
-        <Content></Content>
+        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>
     );
   }
