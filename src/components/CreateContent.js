476fcb35d58c0f0f6fbff6ae1e96751ce83ed8ae
computer99 <pitilesslove@gmail.com>
2022-02-23 15:23:57 +0900

    - create해주는 Component 작성

--- src/components/CreateContent.js
+++ src/components/CreateContent.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react"
 
 class CreateContent extends Component {
-    render() {
-      console.log('CreateContent render');
-      return (
-        <article>
-          <h2>Create Content</h2>
-
-        </article>
-      )
-    }
+  render() {
+    console.log('CreateContent render');
+    return (
+      <article>
+        <h2>Create Content</h2>
+        <form action="/create_process" method="post"
+          onSubmit={function (e) {
+            e.preventDefault();
+          }.bind(this)}
+        >
+          <p><input type="text" name="title" placeholder="title"></input></p>
+          <p>
+            <textarea name="desc" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit"></input>
+          </p>
+        </form>
+      </article>
+    )
   }
+}
 
 export default CreateContent
